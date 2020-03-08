71bafe78a26cb525c2661ce01ef5568aa019eee5
rixiobarrios <rixiobarrios.consulado@gmail.com>
2020-03-08 15:57:58 -0500

    Regressed the changes made to a working app

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,150 +1,32 @@
-// import React, { useState } from 'react';
-// import Splash from './Splash';
-// import { makeStyles, useTheme } from '@material-ui/core/styles';
-// import Card from '@material-ui/core/Card';
-// import CardContent from '@material-ui/core/CardContent';
-// import CardMedia from '@material-ui/core/CardMedia';
-// import Typography from '@material-ui/core/Typography';
-// import { pink } from '@material-ui/core/colors';
-
-// const useStyles = makeStyles(theme => ({
-//   root: {
-//     display: 'flex',
-//   },
-//   details: {
-//     display: 'flex',
-//     flexDirection: 'column',
-//   },
-//   content: {
-//     flex: '1 0 auto',
-//   },
-//   cover: {
-//     width: 151,
-//     backgroundColor: pink,
-//   },
-// }));
-
-// const Home = () => {
-//   const classes = useStyles();
-//   const theme = useTheme();
-//   const [splash, setSplash] = useState(true);
-
-//   const hideSplash = () => {
-//     setSplash(false);
-//   }
-
-//   return (
-//     <>
-//       <Splash hideSplash={hideSplash} splash={splash} />
-//       <section id="home" className={splash ? 'home-hidden' : null}>
-//       <Card className={classes.root}>
-//           <div className={classes.details}>
-//             <CardContent className={classes.content}>
-//               <Typography component="h5" variant="h5">
-//               <a href="/vendor">restaurant 1</a>
-//               </Typography>
-//             </CardContent>
-//           </div>
-//           <CardMedia
-//             className={classes.cover}
-//             // image="/public/images/home-placeholder.jpg"
-//             // title="placeholder image for vendor"
-//           />
-//       </Card>
-
-//           {/* <div className={classes.details}>
-//             <CardContent className={classes.content}>
-//               <a href="/vendor">restaurant 2</a>
-//             </CardContent>
-//           </div>
-//           <CardMedia
-//             className={classes.cover}
-//             image="home-placeholder.jpg"
-//             title="placeholder image for vendor"
-//           />
-
-//           <div className={classes.details}>
-//             <CardContent className={classes.content}>
-//               <a href="/vendor">restaurant 3</a>
-//             </CardContent>
-//           </div>
-//           <CardMedia
-//             className={classes.cover}
-//             image="home-placeholder.jpg"
-//             title="placeholder image for vendor"
-//           />
-
-//           <div className={classes.details}>
-//             <CardContent className={classes.content}>
-//               <a href="/vendor">restaurant 4</a>
-//             </CardContent>
-//           </div>
-//           <CardMedia
-//             className={classes.cover}
-//             image="home-placeholder.jpg"
-//             title="placeholder image for vendor"
-//           />
-
-//           <div className={classes.details}>
-//             <CardContent className={classes.content}>
-//               <a href="/vendor">restaurant 5</a>
-//             </CardContent>
-//           </div>
-//           <CardMedia
-//             className={classes.cover}
-//             image="home-placeholder.jpg"
-//             title="placeholder image for vendor"
-//           /> */}
-//         </section>
-//     </>
-//   );
-// };
-
-// export default Home;
-
 import React, { useState } from 'react';
+import Splash from './Splash';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import Splash from './Splash';
+import { pink } from '@material-ui/core/colors';
 
 const useStyles = makeStyles(theme => ({
-    // root: {
-    //   display: 'flex'
-    // },
+    root: {
+        display: 'flex'
+    },
     details: {
         display: 'flex',
         flexDirection: 'column'
     },
     content: {
-        flex: '1 0 auto',
-        textAlign: 'left'
+        flex: '1 0 auto'
     },
     cover: {
-        width: '100%'
-    },
-    marginAutoContainer: {
-        width: '70%',
-        height: 100,
-        margin: 'auto',
-        display: 'flex',
-        overflow: 'hidden'
-
-        // height: 150
+        width: 151,
+        backgroundColor: pink
     }
 }));
 
-export default function Home() {
+const Home = () => {
     const classes = useStyles();
     const theme = useTheme();
-
     const [splash, setSplash] = useState(true);
 
     const hideSplash = () => {
@@ -154,36 +36,155 @@ export default function Home() {
     return (
         <>
             <Splash hideSplash={hideSplash} splash={splash} />
-        <CardActionArea>
-        <CardActions>
             <section id="home" className={splash ? 'home-hidden' : null}>
-                <Grid className={classes.marginAutoContainer}>
-                    <Grid item xs={8} className={classes.details}>
+                <Card className={classes.root}>
+                    <div className={classes.details}>
                         <CardContent className={classes.content}>
                             <Typography component="h5" variant="h5">
-
-                                        <Link href="/vendor">
-                                            Vendor Name 1
-                                        </Link>
-   
-                            </Typography>
-                            <Typography
-                                variant="subtitle1"
-                                color="textSecondary"
-                            >
-                                123 Happy St Bronx, NY
+                                <a href="/vendor">restaurant 1</a>
                             </Typography>
                         </CardContent>
-                    </Grid>
-                    <Grid item xs={4}>
-                        <img
-                            className={classes.cover}
-                            alt="placeholder vendor image"
-                            src="/images/home-placeholder.jpg"
-                        /></CardActions><CardActionArea>
-                        
-                    </Grid>
-                {/* </Grid>
+                    </div>
+                    <CardMedia
+                        className={classes.cover}
+                        image="/public/images/home-placeholder.jpg"
+                        title="placeholder image for vendor"
+                    />
+                </Card>
+
+                <div className={classes.details}>
+                    <CardContent className={classes.content}>
+                        <a href="/vendor">restaurant 2</a>
+                    </CardContent>
+                </div>
+                <CardMedia
+                    className={classes.cover}
+                    image="home-placeholder.jpg"
+                    title="placeholder image for vendor"
+                />
+
+                <div className={classes.details}>
+                    <CardContent className={classes.content}>
+                        <a href="/vendor">restaurant 3</a>
+                    </CardContent>
+                </div>
+                <CardMedia
+                    className={classes.cover}
+                    image="home-placeholder.jpg"
+                    title="placeholder image for vendor"
+                />
+
+                <div className={classes.details}>
+                    <CardContent className={classes.content}>
+                        <a href="/vendor">restaurant 4</a>
+                    </CardContent>
+                </div>
+                <CardMedia
+                    className={classes.cover}
+                    image="home-placeholder.jpg"
+                    title="placeholder image for vendor"
+                />
+
+                <div className={classes.details}>
+                    <CardContent className={classes.content}>
+                        <a href="/vendor">restaurant 5</a>
+                    </CardContent>
+                </div>
+                <CardMedia
+                    className={classes.cover}
+                    image="home-placeholder.jpg"
+                    title="placeholder image for vendor"
+                />
+            </section>
+        </>
+    );
+};
+
+export default Home;
+
+// import React, { useState } from 'react';
+// import { makeStyles, useTheme } from '@material-ui/core/styles';
+// import Card from '@material-ui/core/Card';
+// import CardContent from '@material-ui/core/CardContent';
+// import CardActionArea from '@material-ui/core/CardActionArea';
+// import CardActions from '@material-ui/core/CardActions';
+// import CardMedia from '@material-ui/core/CardMedia';
+// import Typography from '@material-ui/core/Typography';
+// import Link from '@material-ui/core/Link';
+// import Grid from '@material-ui/core/Grid';
+// import Box from '@material-ui/core/Box';
+// import Splash from './Splash';
+
+// const useStyles = makeStyles(theme => ({
+//     // root: {
+//     //   display: 'flex'
+//     // },
+//     details: {
+//         display: 'flex',
+//         flexDirection: 'column'
+//     },
+//     content: {
+//         flex: '1 0 auto',
+//         textAlign: 'left'
+//     },
+//     cover: {
+//         width: '100%'
+//     },
+//     marginAutoContainer: {
+//         width: '70%',
+//         height: 100,
+//         margin: 'auto',
+//         display: 'flex',
+//         overflow: 'hidden'
+
+//         // height: 150
+//     }
+// }));
+
+// export default function Home() {
+//     const classes = useStyles();
+//     const theme = useTheme();
+
+//     const [splash, setSplash] = useState(true);
+
+//     const hideSplash = () => {
+//         setSplash(false);
+//     };
+
+//     return (
+//         <>
+//             <Splash hideSplash={hideSplash} splash={splash} />
+//         <CardActionArea>
+//         <CardActions>
+//             <section id="home" className={splash ? 'home-hidden' : null}>
+//                 <Grid className={classes.marginAutoContainer}>
+//                     <Grid item xs={8} className={classes.details}>
+//                         <CardContent className={classes.content}>
+//                             <Typography component="h5" variant="h5">
+
+//                                         <Link href="/vendor">
+//                                             Vendor Name 1
+//                                         </Link>
+
+//                             </Typography>
+//                             <Typography
+//                                 variant="subtitle1"
+//                                 color="textSecondary"
+//                             >
+//                                 123 Happy St Bronx, NY
+//                             </Typography>
+//                         </CardContent>
+//                     </Grid>
+//                     <Grid item xs={4}>
+//                         <img
+//                             className={classes.cover}
+//                             alt="placeholder vendor image"
+//                             src="/images/home-placeholder.jpg"
+//                         /></CardActions><CardActionArea>
+
+//                     </Grid>
+{
+    /* </Grid>
                 <Grid className={classes.marginAutoContainer}>
                     <Grid item xs={8} className={classes.details}>
                         <CardContent className={classes.content}>
@@ -227,8 +228,11 @@ export default function Home() {
                             src="/images/home-placeholder.jpg"
                         />
                     </Grid>
-                </Grid> */}
-            </section>
+                </Grid> */
+}
+{
+    /* </section>
         </>
     );
+} */
 }
