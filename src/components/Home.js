1060f4dcf6ae59161df12b9e326be7ac9d848c35
Sage-Kearney <56275819+Sage-Kearney@users.noreply.github.com>
2020-03-07 21:54:42 -0500

    added material.ui extensions to display vendors in a card. not able to get image to show

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,120 @@
 import React, { useState } from 'react';
 import Splash from './Splash';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+// import { pink } from '@material-ui/core/colors';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  content: {
+    flex: '1 0 auto',
+  },
+  cover: {
+    width: 151,
+    // backgroundColor: black,
+  },
+}));
 
 const Home = () => {
+  const classes = useStyles();
+  const theme = useTheme();
   const [splash, setSplash] = useState(true);
 
   const hideSplash = () => {
     setSplash(false);
   }
 
+  
+  
+
   return (
     <>
-    <Splash hideSplash={hideSplash} splash={splash}/>
-    <section id='home' className={splash ? 'home-hidden' : null}>
-      <ul>
-        <li>
-          <a href="/vendor">restaurant 1</a>
-        </li>
-        <li>restaurant 2</li>
-        <li>restaurant 3</li>
-        <li>restaurant 4</li>
-        <li>restaurant 5</li>
-      </ul>
-    </section>
+      <Card className={classes.root}>
+        <Splash hideSplash={hideSplash} splash={splash} />
+        <section id="home" className={splash ? 'home-hidden' : null}>
+          <div className={classes.details}>
+            <CardContent className={classes.content}>
+              <Typography component="h5" variant="h5">
+              <a href="/vendor">restaurant 1</a>
+              </Typography>
+            </CardContent>
+          </div>
+          <CardMedia
+            className={classes.cover}
+            image="/public/images/home-placeholder.jpg"
+            title="placeholder image for vendor"
+          />
+
+          {/* <div className={classes.details}>
+            <CardContent className={classes.content}>
+              <a href="/vendor">restaurant 2</a>
+            </CardContent>
+          </div>
+          <CardMedia
+            className={classes.cover}
+            image="home-placeholder.jpg"
+            title="placeholder image for vendor"
+          />
+
+          <div className={classes.details}>
+            <CardContent className={classes.content}>
+              <a href="/vendor">restaurant 3</a>
+            </CardContent>
+          </div>
+          <CardMedia
+            className={classes.cover}
+            image="home-placeholder.jpg"
+            title="placeholder image for vendor"
+          />
+
+          <div className={classes.details}>
+            <CardContent className={classes.content}>
+              <a href="/vendor">restaurant 4</a>
+            </CardContent>
+          </div>
+          <CardMedia
+            className={classes.cover}
+            image="home-placeholder.jpg"
+            title="placeholder image for vendor"
+          />
+
+          <div className={classes.details}>
+            <CardContent className={classes.content}>
+              <a href="/vendor">restaurant 5</a>
+            </CardContent>
+          </div>
+          <CardMedia
+            className={classes.cover}
+            image="home-placeholder.jpg"
+            title="placeholder image for vendor"
+          /> */}
+        </section>
+      </Card>
     </>
   );
 };
 
 export default Home;
+
+
+{/* <Card className={classes.root}>
+  <div className={classes.details}>
+    <CardContent className={classes.content}>
+
+    </CardContent>
+  </div>
+  <CardMedia
+    className={classes.cover}
+    image="/static/images/cards/live-from-space.jpg"
+    title="Live from space album cover"
+  />
+</Card>; */}
