31dc7b5ebb206541730b28d706ca6ce0b4b0f95a
Qusai Fares <qusaifares@gmail.com>
2020-03-09 16:38:03 -0400

    Updates

---
+++ precache-manifest.73dd258c0ba085791f229f2517dd8135.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b64a6c142bb24fd69c4741b478bf2ac",
+    "url": "/forfoodsake/index.html"
+  },
+  {
+    "revision": "b413b4df0ef2a9fd2a7b",
+    "url": "/forfoodsake/static/css/main.5ad51ced.chunk.css"
+  },
+  {
+    "revision": "f8b338c42dc8fa134d90",
+    "url": "/forfoodsake/static/js/2.252ba3da.chunk.js"
+  },
+  {
+    "revision": "a5288a4ed4e4beba8413aa5c9ae969ea",
+    "url": "/forfoodsake/static/js/2.252ba3da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b413b4df0ef2a9fd2a7b",
+    "url": "/forfoodsake/static/js/main.89b960ed.chunk.js"
+  },
+  {
+    "revision": "fd621a43f61b72226287",
+    "url": "/forfoodsake/static/js/runtime-main.fd9d2408.js"
+  }
+]);
