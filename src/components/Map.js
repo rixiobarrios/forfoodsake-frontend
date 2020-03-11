45e33a77eaaf162cf59fc08f52d4bf546daa7775
Qusai Fares <qusaifares@gmail.com>
2020-03-10 22:12:00 -0400

    added map functionality

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,14 +1,25 @@
 import React from 'react';
 
-const Map = () => {
-  return (
-    <>
-      <img
-        src={`${process.env.PUBLIC_URL}/images/map-placeholder.png`}
-        alt="map"
-      />
-    </>
-  );
+const Map = ({ user }) => {
+    return (
+        <>
+            <iframe
+                title="map"
+                width="450"
+                height="250"
+                frameborder="0"
+                style={{ border: 0 }}
+                src={`https://www.google.com/maps/embed/v1/place?key=${
+                    process.env.REACT_APP_GMAPS_KEY
+                }&q=${encodeURIComponent(
+                    user.street,
+                    user.state,
+                    user.zipCode
+                )}`}
+                allowfullscreen
+            ></iframe>
+        </>
+    );
 };
 
 export default Map;
