d2efc94e474540d82829d425b0d3cddc33b0e379
rixiobarrios <rixiobarrios.consulado@gmail.com>
2020-03-11 15:17:41 -0500

    Building testing file

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,51 +7,51 @@ import PersonIcon from '@material-ui/icons/Person';
 import { withStyles, makeStyles } from '@material-ui/styles';
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    backgroundColor: '#F16642'
-  },
-  navIcon: {
-    color: 'white'
-  }
+    root: {
+        backgroundColor: '#F16642'
+    },
+    navIcon: {
+        color: 'white'
+    }
 }));
 
 const NavBar = ({ user }) => {
-  const classes = useStyles();
-  const [value, setValue] = useState();
-  return (
-    <BottomNavigation
-      value={value}
-      onChange={(event, newValue) => {
-        setValue(newValue);
-      }}
-      showLabels
-      className={classes.root}
-      id="nav"
-    >
-      <BottomNavigationAction
-        component={Link}
-        className={classes.navIcon}
-        to="/login"
-        label="Login"
-        icon={<VpnKeyIcon />}
-      />
-      <BottomNavigationAction
-        component={Link}
-        className={classes.navIcon}
-        to="/"
-        label="Home"
-        icon={<HomeIcon />}
-      />
-      {user ? (
-        <BottomNavigationAction
-          component={Link}
-          className={classes.navIcon}
-          to={`/vendors/${user.id}`}
-          label="Profile"
-          icon={<PersonIcon />}
-        />
-      ) : null}
-    </BottomNavigation>
-  );
+    const classes = useStyles();
+    const [value, setValue] = useState();
+    return (
+        <BottomNavigation
+            value={value}
+            onChange={(event, newValue) => {
+                setValue(newValue);
+            }}
+            showLabels
+            className={classes.root}
+            id="nav"
+        >
+            <BottomNavigationAction
+                component={Link}
+                className={classes.navIcon}
+                to="/login"
+                label="Login"
+                icon={<VpnKeyIcon />}
+            />
+            <BottomNavigationAction
+                component={Link}
+                className={classes.navIcon}
+                to="/"
+                label="Home"
+                icon={<HomeIcon />}
+            />
+            {user ? (
+                <BottomNavigationAction
+                    component={Link}
+                    className={classes.navIcon}
+                    to={`/vendors/${user.id}`}
+                    label="Profile"
+                    icon={<PersonIcon />}
+                />
+            ) : null}
+        </BottomNavigation>
+    );
 };
 export default NavBar;
---
+++ src/components/NavBar.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { NavBar } from './NavBar';
+import {
+    act,
+    renderHook,
+    render,
+    fireEvent
+} from '@testing-library/react-hooks';
