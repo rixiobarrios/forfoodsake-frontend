5727f6cfc08847f2d6a953ac20b8fdeb7c3de073
mmarmol88 <mindysoto88@gmail.com>
2020-03-12 16:53:37 -0400

    Testing CreateListing image upload with aws

--- src/components/FoodListItem.js
+++ src/components/FoodListItem.js
@@ -1,85 +1,80 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import { Link } from 'react-router-dom';
 import {
-    Card,
-    CardActionArea,
-    CardContent,
-    CardMedia,
-    Typography,
-    Box,
-    Grid
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+  Box,
+  Grid
 } from '@material-ui/core/';
 
 const useStyles = makeStyles(() => ({
-    img: {
-        width: '100%'
-    },
-    imgContainer: {
-        width: '100%',
-        height: 200,
-        overflow: 'hidden'
-    },
-    vendorInfo: {
-        borderBottom: '1px solid #bbb',
-        height: '250px',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-evenly',
-        alignItems: 'flex-start',
-        fontSize: '1.3rem',
-        margin: '0 auto',
-        width: 'calc(100% - 4rem)'
-    },
-    description: {
-        margin: '0 auto',
-        fontSize: '1.2rem',
-        lineHeight: '1.4',
-        width: 'calc(100% - 4rem)',
-        padding: '1rem'
-    },
-    media: {
-        height: 200
-    }
+  img: {
+    width: '100%'
+  },
+  imgContainer: {
+    width: '100%',
+    height: 200,
+    overflow: 'hidden'
+  },
+  vendorInfo: {
+    borderBottom: '1px solid #bbb',
+    height: '250px',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+    alignItems: 'flex-start',
+    fontSize: '1.3rem',
+    margin: '0 auto',
+    width: 'calc(100% - 4rem)'
+  },
+  description: {
+    margin: '0 auto',
+    fontSize: '1.2rem',
+    lineHeight: '1.4',
+    width: 'calc(100% - 4rem)',
+    padding: '1rem'
+  },
+  media: {
+    height: 200
+  }
 }));
 
 const FoodListItem = ({ listing, vendor }) => {
-    const classes = useStyles();
-    return (
-        <>
-            <Grid>
-                <Grid item xs={6}>
-                    <Card variant="outlined">
-                        <CardActionArea>
-                            <CardMedia
-                                className={classes.media}
-                                image={listing.image}
-                                title="listing food"
-                            />
-                            <CardContent>
-                                <Typography
-                                    gutterBottom
-                                    variant="h5"
-                                    component="h2"
-                                >
-                                    {listing.name}
-                                </Typography>
-                                <Typography
-                                    gutterBottom
-                                    variant="body2"
-                                    color="textSecondary"
-                                    component="p"
-                                >
-                                    ${listing.price}
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
-                </Grid>
-            </Grid>
-        </>
-    );
+  const classes = useStyles();
+  return (
+    <>
+      <Grid>
+        <Grid item xs={6}>
+          <Card variant="outlined">
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image={listing.image}
+                title="listing food"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  {listing.name}
+                </Typography>
+                <Typography
+                  gutterBottom
+                  variant="body2"
+                  color="textSecondary"
+                  component="p"
+                >
+                  ${listing.price}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+      </Grid>
+    </>
+  );
 };
 
 export default FoodListItem;
