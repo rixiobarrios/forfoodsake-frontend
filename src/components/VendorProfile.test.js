a97eeb7951006c91a4f9bd996036f1c1c27c7fc9
Sage-Kearney <56275819+Sage-Kearney@users.noreply.github.com>
2020-03-11 23:16:08 -0400

    commit before pull

--- src/components/VendorProfile.test.js
+++ src/components/VendorProfile.test.js
@@ -1,29 +1,24 @@
-import React from 'react'; 
+import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import VendorProfile from './VendorProfile';
 
- 
 afterEach(cleanup);
 
-
-
 // test if user can view vendor page *** add data-testid="vendor-page" on box element on line 70
-it ('renders vendor profile page', () => {
-    const {queryByTestId} = render(<VendorProfile/>)
+it('renders vendor profile page', () => {
+  const { queryByTestId } = render(<VendorProfile />);
 
-    expect(queryByTestId('vendor-page')).toBe(true)
-    // expect(queryByTestId('vendor-page')).toBeTruthy();
-})
-
+  expect(queryByTestId('vendor-page')).toBe(true);
+  // expect(queryByTestId('vendor-page')).toBeTruthy();
+});
 
 // page should contain correct vendor name... test get
 test("should render selected vendor's profile page", () => {
-    const selectedVendor = "Dolly's Donuts"
-    const {queryByText} = render(<VendorProfile/>)
+  const selectedVendor = "Dolly's Donuts";
+  const { queryByText } = render(<VendorProfile />);
 
-    expect(queryByText(selectedVendor)).toBe(`Dolly's Donuts`)
-})
-
+  expect(queryByText(selectedVendor)).toBe(`Dolly's Donuts`);
+});
 
 // page should delete correct vendor account... test delete
