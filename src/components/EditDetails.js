0407729cbfb18fefe07c29e83958e54111722242
Qusai Fares <qusaifares@gmail.com>
2020-03-12 00:38:59 -0400

    added editfields component

--- src/components/EditDetails.js
+++ src/components/EditDetails.js
@@ -15,9 +15,37 @@ import {
 } from '@material-ui/core/';
 import FoodListItem from './FoodListItem';
 
+const useStyles = makeStyles(() => ({
+    fields: {
+        marginBottom: 100
+    },
+    card: {
+        marginBottom: 1
+    },
+    fieldName: {
+        textTransform: 'capitalize'
+    },
+    field: {
+        height: 50,
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        fontSize: '1.2rem'
+    }
+}));
+
 const EditDetails = ({ editType, user, match }) => {
+    const classes = useStyles();
     let history = useHistory();
     const [details, setDetails] = useState({});
+    const fieldsToIgnore = useState([
+        'id',
+        'VendorId',
+        'image',
+        'createdAt',
+        'updatedAt',
+        'Listings'
+    ]);
     useEffect(() => {
         if (!user) {
             history.push('/login');
@@ -33,6 +61,9 @@ const EditDetails = ({ editType, user, match }) => {
         // return cleanUp();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [match.params.id, editType, user]);
+    // useEffect(() => {
+    //     return cleanUp();
+    // }, []);
     const cleanUp = () => {
         setDetails(null);
     };
@@ -49,14 +80,44 @@ const EditDetails = ({ editType, user, match }) => {
                 ></Box>
             </label>
             <Box className="fields">
-                {Object.keys(details).map(field => (
-                    <Box className="field">
-                        <Typography className="fieldName">{field}</Typography>
-                        <Typography className="fieldValue">
-                            {details[field]}
-                        </Typography>
-                    </Box>
-                ))}
+                {Object.keys(details).map(field => {
+                    if (!fieldsToIgnore[0].includes(field)) {
+                        console.log(
+                            fieldsToIgnore,
+                            field,
+                            fieldsToIgnore[0].includes(field)
+                        );
+                        return (
+                            <Card className={classes.card}>
+                                <CardActionArea
+                                    key={field}
+                                    className={classes.action}
+                                >
+                                    <CardContent className={classes.field}>
+                                        <Typography
+                                            className={classes.fieldName}
+                                        >
+                                            {field.replace(/_/g, ' ')}
+                                        </Typography>
+                                        <Typography
+                                            className={classes.fieldValue}
+                                        >
+                                            {details[field]}
+                                        </Typography>
+                                    </CardContent>
+
+                                    <CardMedia
+                                        className={classes.cover}
+                                        image={`${process.env.PUBLIC_URL}/images/home-placeholder.jpg`}
+                                        title="placeholder image for vendor"
+                                    />
+                                </CardActionArea>
+                            </Card>
+                        );
+                    } else {
+                        return null;
+                    }
+                })}
             </Box>
         </Box>
     ) : (
