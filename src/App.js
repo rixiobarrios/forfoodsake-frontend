8a17516ba1abf0610afcb24a7eaf61aad99bcb7c
Qusai Fares <qusaifares@gmail.com>
2020-03-12 22:17:46 -0400

    localstorage clears if user doesnt exist

--- src/App.js
+++ src/App.js
@@ -12,122 +12,133 @@ import EditDetails from './components/EditDetails';
 import EditField from './components/EditField';
 
 function App() {
-  const [splash, setSplash] = useState(true);
-  const [user, setUser] = useState();
-  const [listingToEdit, setListingToEdit] = useState();
+    const [splash, setSplash] = useState(true);
+    const [user, setUser] = useState();
+    const [listingToEdit, setListingToEdit] = useState();
 
-  const hideSplash = () => {
-    setSplash(false);
-  };
-  useEffect(() => {
-    setTimeout(() => {
-      hideSplash();
-    }, 1000);
-    console.log(localStorage.userId);
-    if (localStorage.userId) {
-      fetch(`http://localhost:5000/api/vendors/${localStorage.userId}`)
-        .then(res => res.json())
-        .then(data => {
-          let tempUser = data;
-          delete tempUser.password;
-          setUser(tempUser);
-        });
-    }
-  }, []);
-  return (
-    <div className="App">
-      <main className="app-content">
-        <Switch>
-          <Route
-            exact
-            path="/"
-            component={() => <Home hideSplash={hideSplash} splash={splash} />}
-          />
-          <Route
-            exact
-            path="/login"
-            component={() => <AccountForm setUser={setUser} user={user} />}
-          />
-          <Route
-            exact
-            path="/vendors/:id"
-            render={routerProps => (
-              <VendorProfile
-                match={routerProps.match}
-                user={user}
-                setUser={setUser}
-              />
-            )}
-          />
-          <Route
-            exact
-            path="/vendors/:vendorId/listings/:listingId"
-            render={routerProps => (
-              <ListingDetail user={user} match={routerProps.match} />
-            )}
-          />
-          <Route
-            exact
-            path="/newlisting"
-            render={routerProps => {
-              return <CreateListing user={user} />;
-            }}
-          />
-          <Route
-            path="/edit/account"
-            exact
-            render={routerProps => (
-              <EditDetails
-                setListingToEdit={setListingToEdit}
-                editType="vendor"
-                user={user}
-                match={routerProps.match}
-              />
-            )}
-          />
-          <Route
-            path="/edit/listing/:id"
-            exact
-            render={routerProps => (
-              <EditDetails
-                setListingToEdit={setListingToEdit}
-                editType="listing"
-                user={user}
-                match={routerProps.match}
-              />
-            )}
-          />
-          <Route
-            path="/edit/account/:field/"
-            exact
-            render={routerProps => (
-              <EditField
-                editType="vendor"
-                user={user}
-                match={routerProps.match}
-                setUser={setUser}
-              />
-            )}
-          />
-          <Route
-            path="/edit/listing/:id/:field/"
-            exact
-            render={routerProps => (
-              <EditField
-                editType="listing"
-                user={user}
-                match={routerProps.match}
-                setUser={setUser}
-              />
-            )}
-          />
-        </Switch>
-      </main>
-      <footer className="nav-container">
-        <NavBar splash={splash} user={user} />
-      </footer>
-    </div>
-  );
+    const hideSplash = () => {
+        setSplash(false);
+    };
+    useEffect(() => {
+        setTimeout(() => {
+            hideSplash();
+        }, 1000);
+        console.log(localStorage.userId);
+        if (localStorage.userId) {
+            fetch(`http://localhost:5000/api/vendors/${localStorage.userId}`)
+                .then(res => res.json())
+                .then(data => {
+                    if (data) {
+                        let tempUser = data;
+                        delete tempUser.password;
+                        setUser(tempUser);
+                    } else {
+                        localStorage.removeItem('userId');
+                    }
+                });
+        }
+    }, []);
+    return (
+        <div className="App">
+            <main className="app-content">
+                <Switch>
+                    <Route
+                        exact
+                        path="/"
+                        component={() => (
+                            <Home hideSplash={hideSplash} splash={splash} />
+                        )}
+                    />
+                    <Route
+                        exact
+                        path="/login"
+                        component={() => (
+                            <AccountForm setUser={setUser} user={user} />
+                        )}
+                    />
+                    <Route
+                        exact
+                        path="/vendors/:id"
+                        render={routerProps => (
+                            <VendorProfile
+                                match={routerProps.match}
+                                user={user}
+                                setUser={setUser}
+                            />
+                        )}
+                    />
+                    <Route
+                        exact
+                        path="/vendors/:vendorId/listings/:listingId"
+                        render={routerProps => (
+                            <ListingDetail
+                                user={user}
+                                match={routerProps.match}
+                            />
+                        )}
+                    />
+                    <Route
+                        exact
+                        path="/newlisting"
+                        render={routerProps => {
+                            return <CreateListing user={user} />;
+                        }}
+                    />
+                    <Route
+                        path="/edit/account"
+                        exact
+                        render={routerProps => (
+                            <EditDetails
+                                setListingToEdit={setListingToEdit}
+                                editType="vendor"
+                                user={user}
+                                match={routerProps.match}
+                            />
+                        )}
+                    />
+                    <Route
+                        path="/edit/listing/:id"
+                        exact
+                        render={routerProps => (
+                            <EditDetails
+                                setListingToEdit={setListingToEdit}
+                                editType="listing"
+                                user={user}
+                                match={routerProps.match}
+                            />
+                        )}
+                    />
+                    <Route
+                        path="/edit/account/:field/"
+                        exact
+                        render={routerProps => (
+                            <EditField
+                                editType="vendor"
+                                user={user}
+                                match={routerProps.match}
+                                setUser={setUser}
+                            />
+                        )}
+                    />
+                    <Route
+                        path="/edit/listing/:id/:field/"
+                        exact
+                        render={routerProps => (
+                            <EditField
+                                editType="listing"
+                                user={user}
+                                match={routerProps.match}
+                                setUser={setUser}
+                            />
+                        )}
+                    />
+                </Switch>
+            </main>
+            <footer className="nav-container">
+                <NavBar splash={splash} user={user} />
+            </footer>
+        </div>
+    );
 }
 
 export default App;
